58452e84afcb8e7418abc7c482a40d452b284b20
Jonard Manzanes <99409012+Mr-Jonard@users.noreply.github.com>
2023-03-06 13:09:59 +0800

    Add files via upload

---
+++ 2nd.py
@@ -0,0 +1,66 @@
+class Me:
+    def last_name(self):
+        print("My Last Name: Manzanes")
+
+    def first_name(self):
+        print("My First Name: Jonard")
+
+    def middle_name(self):
+        print("My Middle Name: Galos")
+
+    def age(self):
+        print("My Age: 20")
+
+    def skills(self):
+        print("Few of my skills are: Photo Editing, Logo making, and a little of Front-end Development and UI/UX Designing")
+
+    def birth_date(self):
+        current_year = 2023
+        birthdate = "February 18, 2003"
+        age = current_year - 2003
+        print(f"My Birthdate is {birthdate} and my age is {age} yrs. old")
+    
+    def course(self):
+        print("My Course: Bachelor of Science in Information Technology")
+    
+    def section(self):
+        print("My Section: BSIT 1E")
+    
+    def department(self):
+        print("The Department that Im in: Computer Studies Department")
+    
+    def religion(self):
+        print("My Religion: Roman Catholic")
+    
+    def complexion(self):
+        print("My Complexion: Medium brown")
+
+    def height(self):
+        print("My height: 5'5")
+    
+    def weight(self):
+        print("My Weight: 40kg")
+    
+    def city(self):
+        print("The city that Im in: Ormoc City")
+
+    def province(self):
+        print("The Province: Leyte")
+
+
+about = Me()
+about.last_name()
+about.first_name()
+about.middle_name()
+about.age()
+about.skills()
+about.birth_date()
+about.course()
+about.section()
+about.department()
+about.religion()
+about.complexion()
+about.height()
+about.weight()
+about.city()
+about.province()
